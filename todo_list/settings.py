3789f636d9e3a779af4387fbf0310e471a2c5d77
Salvador Borit <salvaborit@gmail.com>
2022-12-31 01:34:17 -0300

    admin panel working, mysql implemented

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_dev_db',
+        'USER': 'todo_dev',
+        'PASSWORD': 'dev_pwd',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
